b56f771d564f9b3cbbb33069ddea6819e88ce28b
jrgermain <jrgermain@github.com>
2018-11-11 14:52:01 -0500

    add link to find nearest treatment center

--- src/routes/UserTreatment.vue
+++ src/routes/UserTreatment.vue
@@ -1,9 +1,29 @@
 <template>
-  <h1> Hello World! </h1>
+  <div>
+    <h1> Hello World! </h1>
+    <b-card class="text-center" title="Emergency resources">
+      <b-button>Call 911</b-button>
+      <b-button v-on:click="getLocation" >Nearest treatment center</b-button>
+    </b-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'userTreatment'
-};
+  name: 'userTreatment',
+  methods: {
+    getHelp: function (position) {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+      window.location.href = "https://findtreatment.samhsa.gov/locator?sAddr=" + lat + "," + long;
+    },
+    getLocation: function () {
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.getHelp);
+      } else {
+        alert("Sorry, this browser does not support geolocation.");
+      }
+    },
+  }
+}
 </script>
