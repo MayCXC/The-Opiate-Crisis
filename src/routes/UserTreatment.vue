ee6b6843caa3792170829d4b9a20f6fb682d376a
jrgermain <jrgermain@github.com>
2018-11-12 10:52:50 -0500

    open maps in new tab

--- src/routes/UserTreatment.vue
+++ src/routes/UserTreatment.vue
@@ -5,7 +5,7 @@
         If you are in immediate danger, contact emergency services as soon as possible by choosing an option below:
       </p>
       <b-button v-on:click="call911">Call 911</b-button>
-      <b-button v-on:click="getLocation">Find a treatment center</b-button>
+      <b-button href="https://www.google.com/maps/dir/?api=1&destination=drug+rehab+center" target="_blank">Find the nearest treatment center</b-button>
     </b-card>
   </div>
 </template>
@@ -18,7 +18,7 @@ export default {
       let lat = position.coords.latitude;
       let long = position.coords.longitude;
       //window.location.href = "https://findtreatment.samhsa.gov/locator?sAddr=" + lat + "," + long;
-      window.location.href = "https://www.google.com/maps/dir/?api=1&destination=drug+rehab+center"
+      //window.location.href = "https://www.google.com/maps/dir/?api=1&destination=drug+rehab+center"
     },
     getLocation: function () {
       if(navigator.geolocation) {
