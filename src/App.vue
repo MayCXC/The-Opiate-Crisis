52e2311508fd29502a65d6d94774dc285c90a639
isolutionja <mcintoshdaron@gmail.com>
2018-11-19 22:19:03 -0500

    deploy to heroku

--- src/App.vue
+++ src/App.vue
@@ -58,6 +58,19 @@
 </template>
 
 <script>
+import Vue from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as VueGoogleMaps from "vue2-google-maps"; // google maps API integration
+import GmapCluster from "vue2-google-maps/dist/components/cluster";
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    // key: "AIzaSyDgHvNq8FAubhXF4lXOeHTCDnMEklu-0G8",
+    key: process.env.VUE_APP_API_KEY,
+    libraries: "places"
+  }
+});
+Vue.component("GmapCluster", GmapCluster);
 export default {
   name: "app",
   data: function() {
--- src/main.js
+++ src/main.js
@@ -1,19 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "bootstrap/dist/css/bootstrap.min.css";
-import * as VueGoogleMaps from "vue2-google-maps"; // google maps API integration
-import GmapCluster from "vue2-google-maps/dist/components/cluster";
-Vue.config.productionTip = false;
 
-Vue.use(VueGoogleMaps, {
-  load: {
-    // key: "AIzaSyDgHvNq8FAubhXF4lXOeHTCDnMEklu-0G8",
-    key: process.env.VUE_APP_API_KEY,
-    libraries: "places"
-  }
-});
-Vue.component("GmapCluster", GmapCluster);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
