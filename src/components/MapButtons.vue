8073311d8266f41666f885f8c9a21f1557c960af
isolutionja <mcintoshdaron@gmail.com>
2018-11-20 19:51:18 -0500

    switch map buttons to primary from secondary

--- src/components/MapButtons.vue
+++ src/components/MapButtons.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="btn-group">
-    <button class="btn btn-secondary" type="button" v-bind:class="{active: active_el == 0}" v-on:click="onClickShowAll(0)">Show All</button>
-    <button class="btn btn-secondary" type="button" v-bind:class="{active: active_el == 1}" v-on:click="onClickSubstance(1)">Subtance Abuse Facilities</button>
-    <button class="btn btn-secondary" type="button" v-bind:class="{active: active_el == 2}" v-on:click="onClickNaloxone(2)">Naloxone Pharmacies</button>
-    <button class="btn btn-secondary" type="button" v-bind:class="{active: active_el == 3}" v-on:click="onClickAlcoholDrug(3)">Alcohol and Drug Counselors</button>
+    <button class="btn btn-primary" type="button" v-bind:class="{active: active_el == 0}" v-on:click="onClickShowAll(0)">Show All</button>
+    <button class="btn btn-primary" type="button" v-bind:class="{active: active_el == 1}" v-on:click="onClickSubstance(1)">Subtance Abuse Facilities</button>
+    <button class="btn btn-primary" type="button" v-bind:class="{active: active_el == 2}" v-on:click="onClickNaloxone(2)">Naloxone Pharmacies</button>
+    <button class="btn btn-primary" type="button" v-bind:class="{active: active_el == 3}" v-on:click="onClickAlcoholDrug(3)">Alcohol and Drug Counselors</button>
   </div>
 </template>
 
@@ -38,7 +38,7 @@ export default {
 </script>
 
 <style>
-.btn-secondary {
+.btn-primary {
   margin: 5px;
 }
 </style>
