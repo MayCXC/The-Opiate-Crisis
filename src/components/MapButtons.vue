4f4a10e9b5b3a563598eab090885cc60a2d6c198
isolutionja <mcintoshdaron@gmail.com>
2018-11-20 19:40:35 -0500

    add active state to buttons

--- src/components/MapButtons.vue
+++ src/components/MapButtons.vue
@@ -1,27 +1,36 @@
 <template>
   <div class="btn-group">
-    <button class="btn btn-secondary" type="button" v-on:click="onClickShowAll">Show All</button>
-    <button class="btn btn-secondary" type="button" v-on:click="onClickSubstance">Subtance Abuse Facilities</button>
-    <button class="btn btn-secondary" type="button" v-on:click="onClickNaloxone">Naloxone Pharmacies</button>
-    <button class="btn btn-secondary" type="button" v-on:click="onClickAlcoholDrug">Alcohol and Drug Counselors</button>
+    <button class="btn btn-secondary" type="button" v-bind:class="{active: active_el == 0}" v-on:click="onClickShowAll(0)">Show All</button>
+    <button class="btn btn-secondary" type="button" v-bind:class="{active: active_el == 1}" v-on:click="onClickSubstance(1)">Subtance Abuse Facilities</button>
+    <button class="btn btn-secondary" type="button" v-bind:class="{active: active_el == 2}" v-on:click="onClickNaloxone(2)">Naloxone Pharmacies</button>
+    <button class="btn btn-secondary" type="button" v-bind:class="{active: active_el == 3}" v-on:click="onClickAlcoholDrug(3)">Alcohol and Drug Counselors</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "MapButtons",
+  data: function() {
+    return {
+      active_el: 0
+    };
+  },
   methods: {
     // google maps navigation API filters
-    onClickSubstance: function() {
+    onClickSubstance: function(el) {
+      this.active_el = el;
       this.$emit("clicked", "SAF");
     },
-    onClickNaloxone: function() {
+    onClickNaloxone: function(el) {
+      this.active_el = el;
       this.$emit("clicked", "NPHAR");
     },
-    onClickAlcoholDrug: function() {
+    onClickAlcoholDrug: function(el) {
+      this.active_el = el;
       this.$emit("clicked", "ADC");
     },
-    onClickShowAll: function() {
+    onClickShowAll: function(el) {
+      this.active_el = el;
       this.$emit("clicked", "");
     }
   }
